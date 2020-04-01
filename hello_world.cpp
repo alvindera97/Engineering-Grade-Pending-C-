700bad4ffd26d5d2c5d7913198057a3531dd66b4
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-04-01 13:08:11 +0100

    Added help text reffering to mamul in the Help section of the top bar

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,10 @@
 // wxWidgets "Hello World" Program
 // For compilers that support precompilation, includes "wx/wx.h".
-#include <wx/wxprec.h>
+#include <wx-3.1/wx/wxprec.h>
+#include <wx-3.1/wx/button.h>
+
 #ifndef WX_PRECOMP
-    #include <wx/wx.h>
+    #include <wx-3.1/wx/wx.h>
 #endif
 
 
@@ -12,44 +14,72 @@ class MyApp : public wxApp {
 };
 
 
-class MyFrame : public wxFrame {
+class MyFrame : public wxFrame {  // defines the options on the top bar of the screen here we have:
     public:
         MyFrame();
     private:
-        void OnHello(wxCommandEvent& event);
-        void OnExit(wxCommandEvent& event);
-        void OnAbout(wxCommandEvent& event);
+        void OnHello(wxCommandEvent& event);  // hello option
+        void OnExit(wxCommandEvent& event);  // exit option
+        void OnAbout(wxCommandEvent& event);  // about option
+        void OnHelp(wxCommandEvent& event); // event option
 };
 
 
 enum {
-    ID_Hello = 1
+    ID_Hello = 1,
+    ID_Course_list = 2,
+    ID_New_Calculation = 3,
+    ID_Save_Calculation=4,
 };
 
-wxIMPLEMENT_APP(MyApp);
-bool MyApp::OnInit() {
+wxIMPLEMENT_APP(MyApp);   // initiate main()
+
+bool MyApp::OnInit() {  // append the OnInit() function
     MyFrame *frame = new MyFrame();
     frame->Show(true);
-    return true;
+    return true;  // this is just to show successfull initialisation
 }
 
-MyFrame::MyFrame() : wxFrame(NULL, wxID_ANY, "Hello World") {
+MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Engineering GP Calculator", wxPoint(100, 100), wxSize(800, 600)) {  // Frame of the program
     wxMenu *menuFile = new wxMenu;
-    menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-                     "Help string shown in status bar for this menu item");
     menuFile->AppendSeparator();
+    menuFile->Append(wxID_NEW);
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_SAVE);
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_SAVEAS);
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_UNDO);
+    menuFile->AppendSeparator();
+    menuFile->Append(wxID_REDO);
+    menuFile->AppendSeparator();
+
     menuFile->Append(wxID_EXIT);
+
+    wxMenu *menuCourses = new wxMenu;
+    menuCourses->Append(ID_Course_list, "&All Courses");
+    menuCourses->AppendSeparator();
+    menuCourses->Append(wxID_ANY, "&New Course");
+
     wxMenu *menuHelp = new wxMenu;
     menuHelp->Append(wxID_ABOUT);
-    wxMenuBar *menuBar = new wxMenuBar;
-    menuBar->Append(menuFile, "&File");
+    menuHelp->AppendSeparator();
+    menuHelp->Append(wxID_HELP);
+
+    wxMenuBar *menuBar = new wxMenuBar;  // create the menubar where menus would live
+
+    // append menus to the menubar
+    menuBar->Append(menuFile, "&File");  
+    menuBar->Append(menuCourses, "&Course");
     menuBar->Append(menuHelp, "&Help");
+
     SetMenuBar( menuBar );
     CreateStatusBar();
     SetStatusText("Welcome to wxWidgets!");
     Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
     Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+    Bind(wxEVT_MENU, &MyFrame::OnHelp, this, wxID_HELP);
 }
 
 void MyFrame::OnExit(wxCommandEvent& event) {
@@ -57,10 +87,14 @@ void MyFrame::OnExit(wxCommandEvent& event) {
 }
 
 void MyFrame::OnAbout(wxCommandEvent& event) {
-    wxMessageBox("This is a wxWidgets Hello World example",
-                 "About Hello World", wxOK | wxICON_INFORMATION);
+    wxMessageBox("This is a programme for calculating the GP of an Engineering student at the Univesity of Benin.",
+                 "GP Calculator V0.0.1", wxOK | wxICON_INFORMATION);
 }
 
 void MyFrame::OnHello(wxCommandEvent& event) {
     wxLogMessage("Hello world from wxWidgets!");
+}
+
+void MyFrame::OnHelp(wxCommandEvent& event) {
+    wxMessageBox("For help on how to use this programme, please use the manual");
 }
