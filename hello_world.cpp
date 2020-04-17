4a78e939098df39629e831f30cb2fa8989ac9157
Nwokolo Godwin Chidera <nwokolo.godwin.chidera@gmail.com>
2020-04-17 02:14:23 +0100

    Added the appropriate display gp to their appropriate componenets and also created ID for display CGPA button

--- hello_world.cpp
+++ hello_world.cpp
@@ -159,6 +159,8 @@ enum {
     ID_CALCULATE_300_LEVEL_GP = 300,
     ID_CALCULATE_400_LEVEL_GP = 400,
     ID_CALCULATE_500_LEVEL_GP = 500,
+
+    ID_CALCULATE_CGPA = 12345,
 };
 
 class GpCalculator : public wxApp {
@@ -515,7 +517,7 @@ MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Engineering Grade Pending Calcu
     course_twelve->SetHint("COURSE CODE");
     course_twelve_grade->SetHint("A");
 
-    wxButton *calculate_gp = new wxButton(this, wxID_ANY, "Display GP", wxPoint(42.5, 445), wxSize(75, 50));
+    wxButton *calculate_gp = new wxButton(this, ID_CALCULATE_100_LEVEL_GP, "Display GP", wxPoint(42.5, 445), wxSize(75, 50));
 
     // 200 Level
     wxString level_text_2 = wxT("200 LEVEL");
@@ -605,7 +607,7 @@ MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Engineering Grade Pending Calcu
     course_twelve_2->SetHint("COURSE CODE");
     course_twelve_grade_2->SetHint("A");
 
-    wxButton *calculate_gp_2 = new wxButton(this, wxID_ANY, "Display GP", wxPoint(207.5, 445), wxSize(75, 50));
+    wxButton *calculate_gp_2 = new wxButton(this, ID_CALCULATE_200_LEVEL_GP, "Display GP", wxPoint(207.5, 445), wxSize(75, 50));
 
     // 300 Level
     wxString level_text_3 = wxT("300 LEVEL");
@@ -695,7 +697,7 @@ MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Engineering Grade Pending Calcu
     course_twelve_3->SetHint("COURSE CODE");
     course_twelve_grade_3->SetHint("A");
 
-    wxButton *calculate_gp_3 = new wxButton(this, wxID_ANY, "Display GP", wxPoint(372.5, 445), wxSize(75, 50));
+    wxButton *calculate_gp_3 = new wxButton(this, ID_CALCULATE_300_LEVEL_GP, "Display GP", wxPoint(372.5, 445), wxSize(75, 50));
 
     // 400 Level
     wxString level_text_4 = wxT("400 LEVEL");
@@ -785,7 +787,7 @@ MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Engineering Grade Pending Calcu
     course_twelve_4->SetHint("COURSE CODE");
     course_twelve_grade_4->SetHint("A");
 
-    wxButton *calculate_gp_4 = new wxButton(this, wxID_ANY, "Display GP", wxPoint(537.5, 445), wxSize(75, 50));
+    wxButton *calculate_gp_4 = new wxButton(this, ID_CALCULATE_400_LEVEL_GP, "Display GP", wxPoint(537.5, 445), wxSize(75, 50));
 
     // 500 Level
     wxString level_text_5 = wxT("500 LEVEL");
@@ -875,7 +877,7 @@ MyFrame::MyFrame() : wxFrame(nullptr, wxID_ANY, "Engineering Grade Pending Calcu
     course_twelve_5->SetHint("COURSE CODE");
     course_twelve_grade_5->SetHint("A");
 
-    wxButton *calculate_gp_5 = new wxButton(this, wxID_ANY, "Display GP", wxPoint(702.5, 445), wxSize(75, 50));
+    wxButton *calculate_gp_5 = new wxButton(this, ID_CALCULATE_500_LEVEL_GP, "Display GP", wxPoint(702.5, 445), wxSize(75, 50));
 
     Bind(wxEVT_MENU, &MyFrame::OnHello, this, ID_Hello);
     Bind(wxEVT_MENU, &MyFrame::OnAbout, this, wxID_ABOUT);
